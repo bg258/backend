bfa976b8a02db49036152e3137ec5afa229973fb
Mark Tzvetoslavov Dochev <txvslvxv@gmail.com>
2024-07-02 14:54:21 +0200

    access list items [not finished]

--- repetition/list_access.py
+++ repetition/list_access.py
@@ -16,3 +16,14 @@ print("The last item of the list: " + str(thislist[-1]))
 # When specifying a range, the return value will be a new list with the specified items. 
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print("Range of indexes: " + str(thislist[2:5]))
+# note: The search will start at index 2 (included) and end at index 5 (not included).
+# by leaving out the start value, the range will start at the first item:
+
+# Example
+# This example returns the items from the beginning to, but NOT including, "kiwi":
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print("Example of ranges: " + str(thislist[:4]))
+
+# by leaving out the end value, the range will go on to the end of the list:
+# this example returns the items from "cherry" to the end:
+print("Another example of ranges:" + str(thislist[2:]))
