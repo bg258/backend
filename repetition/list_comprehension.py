10ea71f9a0c01b5eb8867464b403e8aa565995d4
Mark Tzvetoslavov Dochev <txvslvxv@gmail.com>
2024-07-03 18:55:59 +0200

    List comprension [not finished]

--- repetition/list_comprehension.py
+++ repetition/list_comprehension.py
@@ -14,3 +14,25 @@ print(newlist)
 fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
 newlist = [x for x in fruits if "a" in x]
 print(newlist)
+
+# THE SYNTAX
+# --> newlist = [expression for item in iterable if condition == True]
+# The return value is a new list, leaving the old list unchanged. 
+
+# Condition 
+# The condition is like a filter that only accepts the items that valuate to True 
+newlist = [x for x in fruits if x != "apple"]
+
+# The condition "if x != "apple"" will retur True for all the elements other than "apple", making the new list contain 
+# all fruits except "apple".
+# The condition is optional and can be omitted. 
+newlist = [x for x in fruits]
+
+# ------------------------------------------------------------------------------------------------------------------
+# Iterable
+# The iterable can be any "iterable" object, like a list, tuple, set etc..
+# You can use the range() function to create an iterable
+newlist = [x for x in range(10)]
+
+# same example, but with a condition ---> accept only numbers lower than 5. 
+newlist = [x for x in range(10) if x < 5]
